8fb56c8c7db303977fccb9cf68532d1c3b77ffb5
ArsenySamoylov <111872913+ArsenySamoylov@users.noreply.github.com>
2022-08-29 21:17:27 +0300

    Add files via upload

---
+++ string_functions.cpp
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+int custom_puts( const char *s )
+    {
+    int flag_error = EOF;
+    int i = 0;
+
+    while( s[i] != '\0' )
+        flag_error = putchar(s[i++]);
+
+    flag_error = putchar('\n');
+
+    return flag_error;
+    }
+
+char *custom_strchr( char* s, const char c )
+    {
+    int  i = 0;
+    while( s[i] != '\0' )
+        {
+        if( s[i] == c )
+           return (s + i);
+        i++;
+        }
+
+    return NULL;
+    }
+
+int custom_strlen( const char *s )
+    {
+    int i = 0;
+
+    while( s[i] != '\0' )
+        i++;
+
+    return i;
+    }
+
+char *custom_strcpy( char *destiny, const char *source)
+    {
+    int i = 0;
+    while( (destiny[i] = source[i]) != '\0' )
+        i++;
+
+    return destiny;
+    }
+
+char *custom_strcat( char *s, const char *c )
+    {
+    int len = custom_strlen(s);
+    int i = 0;
+
+    while( (s[len+i] = c[i]) != '\0' )
+        i++;
+
+    return s;
+    }
+
+char *custom_strncat( char *s, const char *c, int n)
+    {
+    int len = custom_strlen(s);
+    int i = 0;
+
+    while( (n--) > 0 && (s[len+i] = c[i]) != '\0')
+        i++;
+
+    return s;
+    }
+
+char *custom_fgets( char *s, int MAX_LEN, FILE *iop)
+    {
+    int i = 0, c = 0;
+
+    while( (--MAX_LEN) > 0 && (c = fgetc(iop) ) != EOF && c != '\n' )
+        s[i++] = c;
+
+    if( MAX_LEN == 0 )
+        return NULL;
+
+    s[i] = '\0';
+
+    return s;
+    }
+
+int custom_strcmp( const char *s1, const char *s2 )
+    {
+    int cmp_len = custom_strlen(s1) - custom_strlen(s2);
+
+    if( cmp_len != 0 )
+        return cmp_len;
+
+    for(int i = 0; i < cmp_len; i++)
+        {
+        if( (s1[i] - s2[i]) != 0 )
+            return (s1[i] - s2[i]);
+        }
+
+    return 0;
+    }
+
+int custom_getline( char *s, int MAX_LEN )
+    {
+    int i = 0, c = 0;
+
+    while( (--MAX_LEN > 0) && (c = getchar()) != EOF && c != '\n' )
+        s[i++] = c;
+
+    s[i] = '\0';
+
+    return i;
+    }
